901d2a7933da68ebe8fe90f969c4d81581444552
AshongCyril <cashong005@st.ug.edu.gh>
2025-07-15 21:35:50 +0000

    Added a Supplier class with basic fields and a constructor

--- Pharmacy Inventory Management/src/Supplier.java
+++ Pharmacy Inventory Management/src/Supplier.java
@@ -1,7 +1,9 @@
 import java.io.Serializable;
 
 public class Supplier implements Serializable {
-    public String name, location, id;
+    public String name;
+    public String location;
+    public String id;
     public int deliveryTimeDays;
 
     public Supplier(String name, String location, String id, int deliveryTimeDays) {
