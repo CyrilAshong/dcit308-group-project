b47e1f3a7ec8ac05bf193588e4f5e33257151af3
Bouffdaddy1z <derrickedus66@gmail.com>
2025-07-15 10:31:01 +0000

    set up stack-based sales log and logging method and added retrieval and filtering support for time-based reports

---
+++ Pharmacy Inventory Management/src/SalesLog.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class SalesLog {
+    private Stack<Transaction> sales = new Stack<>();
+
+    public void logSale(Transaction t) {
+        sales.push(t);
+    }
+
+    public List<Transaction> getSalesInPeriod(Date from, Date to) {
+        List<Transaction> result = new ArrayList<>();
+        for (Transaction t : sales) {
+            try {
+                Date txnDate = new Date(t.timestamp);
+                if (!txnDate.before(from) && !txnDate.after(to)) {
+                    result.add(t);
+                }
+            } catch (Exception ignored) {}
+        }
+        return result;
+    }
+
+    public Stack<Transaction> getAll() {
+        return sales;
+    }
+}
